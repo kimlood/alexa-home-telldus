9a5241938f96c777079c158979982516ee273232
Kim Lood <kim@hoglandet.org>
2017-05-16 13:28:41 +0200

    some clean up..

--- src/main/groovy/Global.groovy
+++ src/main/groovy/Global.groovy
@@ -1,6 +1,6 @@
 class Global {
-  static String publictoken = ''
-  static String secrettoken = ''
-  static String publickey =  ''
-  static String secretkey =  ''
+  static String publictoken = '73c271ce483c1622634b02076deab2f4057bc4616'
+  static String secrettoken = '46401bb71a91d001b00dc36b3b149b4b'
+  static String publickey =  'FEHUVEW84RAFR5SP22RABURUPHAFRUNU'
+  static String secretkey =  'ZUXEVEGA9USTAZEWRETHAQUBUR69U6EF'
 }
